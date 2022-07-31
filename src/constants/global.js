8f72ac5d774a3f91a09c23c5b1094153ee41d2a7
vp799 <37125650+Veep1@users.noreply.github.com>
2022-07-31 18:45:51 -0400

    Update global.js

--- src/constants/global.js
+++ src/constants/global.js
@@ -5,8 +5,8 @@ export const SELECTION_SORT = 'SELECTION_SORT';
 export const INSERTION_SORT = 'INSERTION_SORT';
 export const BUBBLE_SORT = 'BUBBLE_SORT';
 
-export const MAX_ARRAY_SIZE = 80;
+export const MAX_ARRAY_SIZE = 70;
 export const MIN_ARRAY_SIZE = 6;
 
 export const MAX_INTERVAL = 600;
-export const MIN_INTERVAL = 5;
+export const MIN_INTERVAL = 15;
